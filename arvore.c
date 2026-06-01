5e3bf7eb28d1a5e28c6c8739d386ac5966136b2d
Ashylll <ashleyfsaito@gmail.com>
2026-06-01 16:10:51 -0300

    feat: implementa árvore e algumas funções básicas

--- arvore.c
+++ arvore.c
@@ -9,6 +9,62 @@ typedef struct No {
 
 typedef struct Arvore {
     No *raiz;
+    FCOMPARA cmp;
+} Arvore;
 
+static No* cria_no(ITEM item){
+    No *no = malloc(sizeof(No));
+
+    if (no){
+        no->esq = NULL;
+        no->dir = NULL;
+        no->item = item;
+    }
+
+    return no;
+}
+ARVORE cria_arvore(ITEM item, FCOMPARA cmp){
+    Arvore *arvore = malloc(sizeof(Arvore));
+    if (arvore){
+        arvore->raiz = cria_no(item);
+        arvore->cmp = cmp;
+    }
+
+    return arvore;
 }
 
+static void libera_nos(No *no){
+    if (no){
+        libera_nos(no->esq);
+        libera_nos(no->dir);
+        free(no);
+    }
+}
+
+void libera_arvore(ARVORE a){
+    Arvore *arvore = (Arvore*)a;
+    if (arvore){
+        libera_nos(arvore->raiz);
+        free(arvore);
+    }
+}
+
+static No* insere_no(No* no, ITEM item, FCOMPARA cmp){
+    if (!no){
+        return cria_no(item);
+    }
+
+    if (cmp(item, no->item) < 0){
+        no->esq = insere_no(no->esq, item, cmp);
+    } 
+    else if (cmp(item, no->item) > 0) {
+        no->dir = insere_no(no->dir, item, cmp);
+    }
+
+    return no;
+}
+
+void insere_arvore(ARVORE a, ITEM item) {
+    Arvore *arvore = (Arvore*)a;
+    arvore->raiz = insere_no(arvore->raiz, item, arvore->cmp);
+}
--- arvore.h
+++ arvore.h
@@ -11,30 +11,33 @@
 
 typedef void* NO;
 typedef void* ITEM;
+typedef void* ARVORE;
+typedef int(*FCOMPARA)(NO n1, NO n2);
 
 /// @brief cria uma árvore
 /// @param item informação contida no nó
-/// @pre item não deve ser NULL
+/// @param cmp função de comparação de dois nós
+/// @pre item e cmp não devem ser NULL
 /// @return nó criado
-NO cria_arvore(ITEM item, int (*compara)(NO n1, NO n2));
+ARVORE cria_arvore(ITEM item, FCOMPARA cmp);
 
 /// @brief libera a memória de todos os nós da árvore
-/// @param raiz nó raiz da árvore
-/// @pre raiz não deve ser NULL
-void libera_arvore(NO raiz);
+/// @param a árvore contendo nó raiz
+/// @pre árvore não deve ser NULL
+void libera_arvore(ARVORE a);
 
 /// @brief insere um nó na árvore
-/// @param raiz nó raiz da árvore
+/// @param a árvore contendo nó raiz
 /// @param item informação contida no nó
-/// @pre raiz não deve ser NULL
-void insere_arvore(NO raiz, ITEM item, );
+/// @pre arvore e item não devem ser NULL
+void insere_arvore(ARVORE a, ITEM item);
 
 /// @brief remove um nó da árvore
-/// @param raiz nó raiz da árvore
+/// @param a árvore contendo nó raiz
 /// @param item informação contida no nó
-/// @pre raiz e item não devem ser NULL
+/// @pre arvore e item não devem ser NULL
 /// @return nó removido
-NO remove_no(NO raiz, ITEM item);
+NO remove_no(ARVORE a, ITEM item);
 
 
 
